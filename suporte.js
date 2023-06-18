bef238e34836e51ec7ac941febf42ba1a3aaf751
MarcelloBonadias <marcelloebonadias@gmail.com>
2023-06-18 17:01:08 -0300

    finalizar

---
+++ suporte.js
@@ -0,0 +1,21 @@
+
+var menu = document.querySelector('nav ul');
+var menuBar = document.querySelector('nav .menu-icon');
+var iconMenu = document.querySelector('nav .menu-icon img');
+
+menuBar.addEventListener('click',function(){
+   
+    if (iconMenu.getAttribute("src") == 'imagens/menuicon.png'){
+        iconMenu.setAttribute("src", "imagens/closeicon.png");
+    }else{
+        iconMenu.setAttribute("src", "imagens/menuicon.png")
+    }
+    menu.classList.toggle('active')
+});
+
+
+
+
+
+
+
